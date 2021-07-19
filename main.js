0824290b3835570a4f406a306cbef7d1f972f605
Mega-Charizard <81663277+Mega-Charizard@users.noreply.github.com>
2021-07-18 21:41:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+ var x= document.getElementById("myAudio");
+
+ var y= new Audio("Bear Creek Dr 3.m4a");
+
+
+function playSound()
+{
+    y.play();
+}
+
+
+
